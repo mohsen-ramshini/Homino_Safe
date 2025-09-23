4e0bc12d6113ee481780d9e36bd8403de2eb2d4d
mohsen-ramshini <rsmohsen20@gmail.com>
2025-09-23 14:25:42 +0330

    fix: remove kbar imports

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,4 +1,3 @@
-import KBar from '@/components/kbar';
 import AppSidebar from '@/components/layout/app-sidebar';
 import Header from '@/components/layout/header';
 import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
@@ -22,7 +21,7 @@ export default async function DashboardLayout({
   const cookieStore = await cookies();
   const defaultOpen = cookieStore.get("sidebar_state")?.value === "true"
   return (
-    // <KBar>
+
           <SidebarProvider defaultOpen={defaultOpen}>
             <LayoutSidebarProvider>
             <AppSidebar />
@@ -35,6 +34,6 @@ export default async function DashboardLayout({
             </SidebarInset>
             </LayoutSidebarProvider>
           </SidebarProvider>
-    // </KBar>
+
   );
 }
